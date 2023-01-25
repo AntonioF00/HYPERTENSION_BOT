925080a43b332e5e487c8ca74b00f47d48514f69
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-01-26 00:31:40 +0100

    update

--- hypertension_bot/Program.cs
+++ hypertension_bot/Program.cs
@@ -1,50 +1,46 @@
-﻿using hypertension_bot.Settings;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using hypertension_bot.Loggers;
+using hypertension_bot.Settings;
+using System.Runtime.CompilerServices;
 using Telegram.Bot;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace hypertension_bot
 {
     class program
     {
-        private static TelegramBotClient _telegramBot = new TelegramBotClient(Setting.Istance.Configuration.BotToken);
+        static TelegramBotClient _telegramBot = new TelegramBotClient(Setting.Istance.Configuration.BotToken);
 
         static void Main(string[] args)
         {
-            var _receiverOptions = new ReceiverOptions
+            try
             {
-                AllowedUpdates = new UpdateType[]
-                {
-                    UpdateType.Message,
-                    UpdateType.EditedMessage,
-                }
-            };
+                _telegramBot.StartReceiving();
+                _telegramBot.OnMessage += Bot_OnMessage;
 
-            _telegramBot.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions);
+            }catch(Exception ex)
+            {
+                LogHelper.Log($"{System.DateTime.Now} | {ex.Message}");
+            }
 
             Console.ReadLine();
         }
 
-        private static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3) => throw new NotImplementedException();
-
-        private static async Task UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken arg3)
+        public static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-                if (update.Type == UpdateType.Message)
+            if(e.Message.Type == MessageType.Text)
+            {
+                if(MessageType.Text.Equals("Ciao"))
                 {
-                    if (update.Message.Type == MessageType.Text)
-                    {
-                        var text = update.Message.Text;
-                        var id = update.Message.Chat.Id;
-                        string? username = update.Message.Chat.Username;
-
-                        Console.WriteLine($"username : {username} | id : {id} | text : {text}");
-                    }
+                    _telegramBot.SendTextMessageAsync(e.Message.Chat.Id, $"Hello {e.Message.Chat.Username}");
                 }
+                else
+                {
+                    _telegramBot.SendTextMessageAsync(e.Message.Chat.Id, $"Sorry i don't understand u...");
+                }
+            }
+
         }
+
     }
 }
 
