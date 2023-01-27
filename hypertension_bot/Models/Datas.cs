636e3b1b1b6d8d5e5df968a1429cd32073f0873c
Antonio Florio <AFlorio45@gmail.com>
2023-01-27 10:43:24 +0100

    update

--- hypertension_bot/Models/Datas.cs
+++ hypertension_bot/Models/Datas.cs
@@ -30,12 +30,12 @@ namespace hypertension_bot.Models
 
         public int BlockLevel { get => _blockLevel; set => _blockLevel = 0; }
         public bool MessDeleted { get => _messDeleted; set => _messDeleted = false; }
-        public int Year { get => _year; set => _year = int.Parse(DateTime.UtcNow.Year.ToString()); }
-        public int Month { get => _month; set => _month = int.Parse(DateTime.UtcNow.Month.ToString()); }
-        public int Day { get => _day; set => _day = int.Parse(DateTime.UtcNow.Day.ToString()); }
-        public int Hour { get => _hour; set => _hour = int.Parse(DateTime.UtcNow.Hour.ToString()); }
-        public int Minute { get => _minute; set => _minute = int.Parse(DateTime.UtcNow.Minute.ToString()); }
-        public int Second { get => _second; set => _second = int.Parse(DateTime.UtcNow.Second.ToString()); }
+        public int Year { get => _year; set => int.Parse(System.DateTime.UtcNow.Year.ToString()); }
+        public int Month { get => _month; set => int.Parse(System.DateTime.UtcNow.Month.ToString()); }
+        public int Day { get => _day; set => int.Parse(System.DateTime.UtcNow.Day.ToString()); }
+        public int Hour { get => _hour; set => int.Parse(System.DateTime.UtcNow.Hour.ToString()); }
+        public int Minute { get => _minute; set => int.Parse(System.DateTime.UtcNow.Minute.ToString()); }
+        public int Second { get => _second; set => int.Parse(System.DateTime.UtcNow.Second.ToString()); }
         public long ChatId { get => _chatId; set => _chatId = value; }
         public string MessageText { get => _messageText; set => _messageText = value; }
         public int MessageId { get => _messageId; set => _messageId = value; }
