974f65e46a4c69d2bdcee1945269e29a5b97a7a2
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-01-30 22:30:54 +0100

    update

--- hypertension_bot/Program.cs
+++ hypertension_bot/Program.cs
@@ -97,7 +97,6 @@ namespace hypertension_bot
             //set variables
             _data.ChatId         = update.Message.Chat.Id;
             _data.MessageText    = update.Message.Text.ToLower();
-            _data.MessageId      = update.Message.MessageId;
             _data.FirstName      = update.Message.From.FirstName;
             _data.LastName       = update.Message.From.LastName;
             _data.Id             = update.Message.From.Id;
