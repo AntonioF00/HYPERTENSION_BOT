17e9162c860f6b544259b89401a0b852d3274a9d
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-01-31 17:27:39 +0100

    update

--- hypertension_bot/Database/viste.sql
+++ hypertension_bot/Database/viste.sql
@@ -5,8 +5,8 @@
     "Measurement".systolic,
     "Measurement".diastolic,
     "Measurement"."heartRate",
-	AVG(systolic)  FILTER (WHERE datetime::timestamp BETWEEN now()::timestamp - '1 month'::interval and now()::timestamp)::int as monthSystolic,
-	AVG(diastolic) FILTER (WHERE datetime::timestamp BETWEEN now()::timestamp - '1 month'::interval and now()::timestamp)::int as monthDiastolic
+	AVG(systolic)  FILTER (WHERE datetime::timestamp BETWEEN (now()::timestamp - '1 month'::interval)::timestamp and now()::timestamp)::int as monthSystolic,
+	AVG(diastolic) FILTER (WHERE datetime::timestamp BETWEEN (now()::timestamp - '1 month'::interval)::timestamp and now()::timestamp)::int as monthDiastolic
    FROM users."Measurement"
   GROUP BY "Measurement"."User_idUser","Measurement".datetime,"Measurement".systolic,"Measurement".diastolic,"Measurement"."heartRate";
 ALTER TABLE users.vw_avguser
