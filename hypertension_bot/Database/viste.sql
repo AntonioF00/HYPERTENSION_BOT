bb01f139245b09652136b5e881bc2f276f9bd8f5
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-01-31 16:44:11 +0100

    update

---
+++ hypertension_bot/Database/viste.sql
@@ -0,0 +1,16 @@
+﻿-- View: users.vw_avguser
+
+-- DROP VIEW users.vw_avguser;
+
+CREATE OR REPLACE VIEW users.vw_avguser
+ AS
+ SELECT DISTINCT ON ("Measurement"."User_idUser") "Measurement"."User_idUser",
+    "Measurement".datetime::date AS datetime,
+    "Measurement".systolic,
+    "Measurement".diastolic,
+    "Measurement"."heartRate"
+   FROM users."Measurement"
+  ORDER BY "Measurement"."User_idUser", ("Measurement".datetime::date) DESC;
+
+ALTER TABLE users.vw_avguser
+    OWNER TO postgres;
