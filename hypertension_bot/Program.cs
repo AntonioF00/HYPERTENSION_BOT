037841e052e11a59fb70a3d541a33de3be37278f
Antonio Florio <AFlorio45@gmail.com>
2023-01-31 12:41:52 +0100

    update

--- hypertension_bot/Program.cs
+++ hypertension_bot/Program.cs
@@ -107,7 +107,7 @@ namespace hypertension_bot
                 _unknown = true;
 
                 _data.SentMessage = await botClient.SendTextMessageAsync(chatId: _data.ChatId,
-                                                                         text: $"{_data.HelloMessage.ReplyMessages[_rnd.Next(4)]} {_data.FirstName}!\n{_data.FirstName}, ti va di dirmi i tuoi valori di oggi? \n(scrivimeli in questo modo...\nad esempio '120 30'...\nGRAZIE!)",
+                                                                         text: $"{_data.HelloMessage.ReplyMessages[_rnd.Next(4)]} {_data.FirstName}!\n{_data.FirstName}, ti va di dirmi i tuoi valori di oggi? \nScrivimeli in questo modo\nAd esempio 120 30\nGrazie!)",
                                                                          cancellationToken: cancellationToken);
             }
             else if (_data.MessageText.Any(char.IsDigit))
