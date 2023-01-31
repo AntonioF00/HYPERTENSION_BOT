e40bdf8c40614f3efcbb624a93ff55d711e198b8
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-01-31 17:17:17 +0100

    update

--- hypertension_bot/Database/viste.sql
+++ hypertension_bot/Database/viste.sql
@@ -1,16 +1,13 @@
-﻿-- View: users.vw_avguser
-
--- DROP VIEW users.vw_avguser;
-
-CREATE OR REPLACE VIEW users.vw_avguser
+﻿CREATE OR REPLACE VIEW users.vw_avguser
  AS
  SELECT DISTINCT ON ("Measurement"."User_idUser") "Measurement"."User_idUser",
-    "Measurement".datetime::date AS datetime,
+    "Measurement".datetime::date,
     "Measurement".systolic,
     "Measurement".diastolic,
-    "Measurement"."heartRate"
+    "Measurement"."heartRate",
+	AVG(systolic)  FILTER (WHERE datetime::timestamp BETWEEN now()::timestamp - '1 month'::interval and now()::timestamp)::int as monthSystolic,
+	AVG(diastolic) FILTER (WHERE datetime::timestamp BETWEEN now()::timestamp - '1 month'::interval and now()::timestamp)::int as monthDiastolic
    FROM users."Measurement"
-  ORDER BY "Measurement"."User_idUser", ("Measurement".datetime::date) DESC;
-
+  GROUP BY "Measurement"."User_idUser","Measurement".datetime,"Measurement".systolic,"Measurement".diastolic,"Measurement"."heartRate";
 ALTER TABLE users.vw_avguser
     OWNER TO postgres;
