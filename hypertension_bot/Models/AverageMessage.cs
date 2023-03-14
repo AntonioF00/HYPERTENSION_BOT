d079df44cf7cf673f86ec07672298bfacbac4960
Antonio Florio <AFlorio45@gmail.com>
2023-03-14 10:00:26 +0100

    bug fix avarageMessage.cs
    Ho eliminato la variabile isEmpty, che non serve a nulla se non a introdurre un branching in più nel codice.
    Ho sostituito la costruzione della stringa s in un'unica istruzione if-else con una concatenazione di stringhe, eliminando codice duplicato.
    Ho sostituito la creazione di una nuova istanza di string con la creazione di una stringa letterale, che è più efficiente perché non richiede l'allocazione di memoria.
    Ho usato il metodo TryGetValue invece di accedere direttamente all'elemento "systolic" del dizionario res, in modo da evitare un'eccezione nel caso in cui l'elemento non esista.
    Ho usato il metodo int.TryParse invece di int.Parse per convertire la stringa "systolic" in un intero, in modo da gestire eventuali errori di conversione.
    Ho spostato la definizione delle variabili valoreMaxSi e valoreMinSi all'interno del blocco if, in modo da non doverle definire se non ci sono valori da elaborare.

--- hypertension_bot/Models/AverageMessage.cs
+++ hypertension_bot/Models/AverageMessage.cs
@@ -9,26 +9,46 @@ namespace hypertension_bot.Models
     {
         private readonly DbController _dbController = new DbController();
 
-        public string calculateMonthAVG(long id, string Name)
+        public string calculateMonthAVG(long id, string name)
         {
-           string s;
-           bool isEmpty;
-           var res = _dbController.CalculateMonthAVG(id);
-           using (var dictionaryEnum = res.GetEnumerator())
-           {
-             isEmpty = !dictionaryEnum.MoveNext();
-           }
-           if (!isEmpty)
-           {
-               var sistolic = int.Parse(string.Format("{0}", res["systolic"]));
-               s = (sistolic >= Setting.Istance.Configuration.ValoreMaxSi || sistolic < Setting.Istance.Configuration.ValoreMinSi)
-                                                                                 ? $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nLa tua media non mi piace!\nDovresti chiamare il medico!"
-                                                                                 : $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nA presto!";
-           }
-           else
-                s = new string("non ho trovato alcun valore! mi dispiace...");
-           return s; 
+            var res = _dbController.CalculateMonthAVG(id);
+            if (res.TryGetValue("systolic", out var systolicStr) &&
+                int.TryParse(systolicStr.ToString(), out var sistolic))
+            {
+                var valoreMaxSi = Setting.Istance.Configuration.ValoreMaxSi;
+                var valoreMinSi = Setting.Istance.Configuration.ValoreMinSi;
+                var s = $"{name} ho trovato questi valori!\nMedia mensile:\nSistolica: {sistolic} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca: {res["heartrate"]} bpm\n" +
+                        (sistolic >= valoreMaxSi || sistolic < valoreMinSi
+                            ? "La tua media non mi piace!\nDovresti chiamare il medico!"
+                            : "A presto!");
+                return s;
+            }
+            else
+            {
+                return "Non ho trovato alcun valore! Mi dispiace...";
+            }
         }
+
+        //public string calculateMonthAVG(long id, string Name)
+        //{
+        //   string s;
+        //   bool isEmpty;
+        //   var res = _dbController.CalculateMonthAVG(id);
+        //   using (var dictionaryEnum = res.GetEnumerator())
+        //   {
+        //     isEmpty = !dictionaryEnum.MoveNext();
+        //   }
+        //   if (!isEmpty)
+        //   {
+        //       var sistolic = int.Parse(string.Format("{0}", res["systolic"]));
+        //       s = (sistolic >= Setting.Istance.Configuration.ValoreMaxSi || sistolic < Setting.Istance.Configuration.ValoreMinSi)
+        //                                                                         ? $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nLa tua media non mi piace!\nDovresti chiamare il medico!"
+        //                                                                         : $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nA presto!";
+        //   }
+        //   else
+        //        s = new string("non ho trovato alcun valore! mi dispiace...");
+        //   return s; 
+        //}
 
         public string calculateWeekAVG(long id, string Name)
         {
