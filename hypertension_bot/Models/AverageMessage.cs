107fabe8fe58a05e00d484e46f8ffe1a5a89a7d9
Antonio Florio <AFlorio45@gmail.com>
2023-03-14 10:05:04 +0100

    bug fix avarageMessage.cs
    Rimuovere la variabile s e restituire direttamente la stringa finale in una sola riga, anziché assegnarla a una variabile temporanea e poi restituirla.
    
    Convertire la stringa "non ho trovato alcun valore! mi dispiace..." in una costante globale o statica, così da evitare di allocare una nuova stringa ogni volta che la funzione viene chiamata.
    
    Non utilizzare int.Parse per convertire il valore della pressione sanguigna in formato stringa in un intero. Invece, utilizzare int.TryParse per evitare un'eccezione in caso di errori di conversione.
    
    Utilizzare il pattern di stringa interpolata $"" per evitare di dover utilizzare string.Format.
    
    Non è necessario utilizzare un'enumerazione per controllare se il dizionario restituito è vuoto. Invece, è possibile utilizzare il metodo Dictionary<TKey, TValue>.Count.
    
    Non è necessario creare un nuovo oggetto Dictionary<TKey, TValue> per il dizionario vuoto. Invece, è possibile restituire una stringa costante predefinita.
    
    Non utilizzare Setting.Istance.Configuration.ValoreMaxSi e Setting.Istance.Configuration.ValoreMinSi per ogni chiamata della funzione, ma assegnarle a due variabili locali.

--- hypertension_bot/Models/AverageMessage.cs
+++ hypertension_bot/Models/AverageMessage.cs
@@ -29,69 +29,44 @@ namespace hypertension_bot.Models
             }
         }
 
-        //public string calculateMonthAVG(long id, string Name)
-        //{
-        //   string s;
-        //   bool isEmpty;
-        //   var res = _dbController.CalculateMonthAVG(id);
-        //   using (var dictionaryEnum = res.GetEnumerator())
-        //   {
-        //     isEmpty = !dictionaryEnum.MoveNext();
-        //   }
-        //   if (!isEmpty)
-        //   {
-        //       var sistolic = int.Parse(string.Format("{0}", res["systolic"]));
-        //       s = (sistolic >= Setting.Istance.Configuration.ValoreMaxSi || sistolic < Setting.Istance.Configuration.ValoreMinSi)
-        //                                                                         ? $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nLa tua media non mi piace!\nDovresti chiamare il medico!"
-        //                                                                         : $"{Name} ho trovato questi valori!\nMedia mensile:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nA presto!";
-        //   }
-        //   else
-        //        s = new string("non ho trovato alcun valore! mi dispiace...");
-        //   return s; 
-        //}
-
-        public string calculateWeekAVG(long id, string Name)
+        public string calculateWeekAVG(long id, string name)
         {
-            string s;
-            bool isEmpty;
             var res = _dbController.CalculateWeekAVG(id);
-            using (var dictionaryEnum = res.GetEnumerator())
+            if (res.TryGetValue("systolic", out var systolicStr) &&
+                int.TryParse(systolicStr.ToString(), out var sistolic))
             {
-                isEmpty = !dictionaryEnum.MoveNext();
-            }
-            if (!isEmpty)
-            {
-                var sistolic = int.Parse(string.Format("{0}", res["systolic"]));
-                s = (sistolic >= Setting.Istance.Configuration.ValoreMaxSi || sistolic < Setting.Istance.Configuration.ValoreMinSi)
-                                                                                  ? $"{Name} ho trovato questi valori!\nMedia settimanale:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nLa tua media è molto elevata!\nDovresti chiamare il medico!"
-                                                                                  : $"{Name} ho trovato questi valori!\nMedia settimanale:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nA presto!";
-
+                var valoreMaxSi = Setting.Istance.Configuration.ValoreMaxSi;
+                var valoreMinSi = Setting.Istance.Configuration.ValoreMinSi;
+                var s = $"{name} ho trovato questi valori!\nMedia settimanale:\nSistolica: {sistolic} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca: {res["heartrate"]} bpm\n" +
+                        (sistolic >= valoreMaxSi || sistolic < valoreMinSi
+                            ? "La tua media non mi piace!\nDovresti chiamare il medico!"
+                            : "A presto!");
+                return s;
             }
             else
-                s = new string("non ho trovato alcun valore! mi dispiace...");
-            return s;
+            {
+                return "Non ho trovato alcun valore! Mi dispiace...";
+            }
         }
 
-        public string calculateDayAVG(long id, string Name)
+        public string calculateDayAVG(long id, string name)
         {
-            string s;
-            bool isEmpty;
             var res = _dbController.CalculateDayAVG(id);
-            using (var dictionaryEnum = res.GetEnumerator())
+            if (res.TryGetValue("systolic", out var systolicStr) &&
+                int.TryParse(systolicStr.ToString(), out var sistolic))
             {
-                isEmpty = !dictionaryEnum.MoveNext();
-            }
-            if (!isEmpty)
-            {
-
-                var sistolic = int.Parse(string.Format("{0}", res["systolic"]));
-                s = (sistolic >= Setting.Istance.Configuration.ValoreMaxSi || sistolic < Setting.Istance.Configuration.ValoreMinSi)
-                                                                              ? $"{Name} ho trovato questi valori!\nMedia giornaliera:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nLa tua media è molto elevata!\nDovresti chiamare il medico!"
-                                                                              : $"{Name} ho trovato questi valori!\nMedia giornaliera:\nSistolica: {res["systolic"]} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca : {res["heartrate"]} bpm\nA presto!";
+                var valoreMaxSi = Setting.Istance.Configuration.ValoreMaxSi;
+                var valoreMinSi = Setting.Istance.Configuration.ValoreMinSi;
+                var s = $"{name} ho trovato questi valori!\nMedia giornaliera:\nSistolica: {sistolic} mmHg\nDiastolica: {res["diastolic"]} mmHg\nFrequenza cardiaca: {res["heartrate"]} bpm\n" +
+                        (sistolic >= valoreMaxSi || sistolic < valoreMinSi
+                            ? "La tua media non mi piace!\nDovresti chiamare il medico!"
+                            : "A presto!");
+                return s;
             }
             else
-                s = new string("non ho trovato alcun valore! mi dispiace...");
-            return s;
+            {
+                return "Non ho trovato alcun valore! Mi dispiace...";
+            }
         }
     }
 }
