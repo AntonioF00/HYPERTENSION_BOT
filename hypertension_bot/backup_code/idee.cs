fce60e742ef5daa17db8710b378c833bf20d1780
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-03-19 13:56:35 +0100

    bug fix

--- hypertension_bot/backup_code/idee.cs
+++ hypertension_bot/backup_code/idee.cs
@@ -1,5 +1,5 @@
-﻿//1. sviluppare un background Worker per gestire la questione della notifica qualora l'utente non avesse fatto misurazioni nell'arco delle ultime 72 ore.
-//2. mettere online la dashboard di Riccardo
-//3. trovare un modo per costruire dei grafici con c#
-//4. dividere il messagio esplicativo su come misurare la pressione
-//5. tentare una implementazioni di qualche intelligenza artificiale per risolvere tutte quelle conversazioni che escono dal contesto del progetto
+﻿1. sviluppare un background Worker per gestire la questione della notifica qualora l'utente non avesse fatto misurazioni nell'arco delle ultime 72 ore.
+2. mettere online la dashboard di Riccardo
+3. trovare un modo per costruire dei grafici con c#
+4. dividere il messagio esplicativo su come misurare la pressione
+5. tentare una implementazioni di qualche intelligenza artificiale per risolvere tutte quelle conversazioni che escono dal contesto del progetto
