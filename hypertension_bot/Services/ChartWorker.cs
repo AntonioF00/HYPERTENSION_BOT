bc57008632a149d3a43afffad35ff21e1ca5af69
Antonio Florio <AFlorio45@gmail.com>
2023-03-22 11:56:24 +0100

    update

--- hypertension_bot/Services/ChartWorker.cs
+++ hypertension_bot/Services/ChartWorker.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace hypertension_bot.Services
 {
     internal class ChartWorker
     {
+        public ChartWorker() { }
+
+        public void Run()
+        {
+
+        }
     }
 }
